7c83274733b7142a2e796047b68f564caf977df1
Rodrigo Garcia <rodrigo@enlightened.org>
2013-10-13 13:25:18 -0700

    adding route to index

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Collective::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'application#index'
+  root :to => 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
