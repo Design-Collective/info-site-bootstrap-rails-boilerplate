b9158f62c52192b67a3c400c5c1e1575d376d04e
Rodrigo Garcia <rodrigo@enlightened.org>
2013-10-12 23:29:52 -0700

    added field info for mercury

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,12 @@
 class Page < ActiveRecord::Base
+  MERCURY_FIELDS = [:page_title, :body]
 
+  def save_from_mercury(params)
+    MERCURY_FIELDS.each do |field|
+      if params && params[:content] && params[:content][field] && params[:content][field][:value]
+        send "#{field}=", params[:content][field][:value]
+      end
+    end
+    save!
+  end
 end
