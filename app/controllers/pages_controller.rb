263bbfd324a433d5f724ec126dc997316e53ab43
Rodrigo Garcia <rodrigo@enlightened.org>
2013-10-12 23:31:02 -0700

    defined all actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,26 @@
 class PagesController < ActionController::Base
+  layout 'application'
 
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def create
+    @page = Page.new(page_params)
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:meta_title, :meta_description, :page_title, :header_image, :body)
+  end
 end
