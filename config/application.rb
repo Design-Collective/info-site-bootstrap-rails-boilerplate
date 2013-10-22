4be636fee3a4b25a815fd38f989da637db1dfd04
Rodirgo <rodrigo@enlightened.org>
2013-10-21 23:56:59 -0700

    require dotenv

--- config/application.rb
+++ config/application.rb
@@ -3,10 +3,12 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 
+unless Rails.env.production?
+  require "dotenv"
+  Dotenv.load
+end
 
-Dotenv.load unless Rails.env.production?
 
-require 'dotenv'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
